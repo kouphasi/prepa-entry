7da9bf845663da2acfb0d5f659730e771b5a8fcd
phasi <koumiyom4@gmail,com>
2022-02-16 17:29:59 +0900

    コメントの追加

--- src/views/CheackPage.vue
+++ src/views/CheackPage.vue
@@ -20,7 +20,7 @@ import { db } from "../firebase.js";
 export default {
   data() {
     return {
-      firedocs: [],
+      firedocs: [], //代入される値の型は[ { "id": "8CcycWJo8GDKB1LXPiEs", "name": "a", "date": "2022-02-15", "university": "慶應", "grade": 2 }, ... ]
     };
   },
   created: function () {
